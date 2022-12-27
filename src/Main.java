55fce481751e90dce42d387f2cfdbe0749041822
Имя <vitya.klyucherov@mail.ru>
2022-12-27 13:00:26 +0300

    Дз. Объекты и классы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
-public class Main {
+public class Main{
+
+
+
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Дз. Объекты и классы");
+
+        Book data0 = new Book("Хроники заводной птицы", 1992);
+        System.out.println("Название книги - " + data0.getTitle());
+        data0.setYear(1995);
+        System.out.println("Год издания - " + data0.getYear() + "г");
+
+        Author name0 =  new Author("Харуки Мураками");
+        System.out.println("Автор - " + name0.getName());
+
+        System.out.println("========================");
+        System.out.println("========================");
+
+        Book data1 = new Book("100 лет одиночества", 1967);
+        System.out.println("Название книги - " + data1.getTitle());
+        System.out.println("Год издания - " + data1.getYear() + "г");
+
+
+        Author name1 = new Author("Габриэля Гарсиа Маркеса");
+        System.out.println("Автор - " + name1.getName());
+
     }
+
 }
